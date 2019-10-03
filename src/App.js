8da26221f19af0c9f0ee503616812b6f53958f09
David Bernhard <davidbernhard@live.com>
2019-10-03 22:14:31 +0100

    all tests passed!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,77 @@
 import React, { useState } from 'react';
+import marked from 'marked';
+
+const renderer = new marked.Renderer();
+renderer.link = function(href, title, text) {
+  let link = marked.Renderer.prototype.link.call(this, href, title, text);
+  return link.replace('<a', "<a target='_blank' ");
+};
+marked.setOptions({
+  renderer: renderer,
+  gfm: true,
+  breaks: true
+});
 
 function App() {
-  const [markdown, setMarkdown] = useState('This is just some text for now.');
+  const [
+    markdown,
+    setMarkdown
+  ] = useState(`# Welcome to my React Markdown Previewer!
+
+  ## This is a sub-heading...
+  ### And here's some other cool stuff:
+    
+  Heres some code, \`<div></div>\`, between 2 backticks.
+  
+  \`\`\`
+  // this is multi-line code:
+  
+  function anotherExample(firstLine, lastLine) {
+    if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+      return multiLineCode;
+    }
+  }
+  \`\`\`
+    
+  You can also make text **bold**... whoa!
+  Or _italic_.
+  Or... wait for it... **_both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
+  
+  There's also [links](https://www.freecodecamp.com), and
+  > Block Quotes!
+  
+  And if you want to get really crazy, even tables:
+  
+  Wild Header | Crazy Header | Another Header?
+  ------------ | ------------- | ------------- 
+  Your content can | be here, and it | can be here....
+  And here. | Okay. | I think we get it.
+  
+  - And of course there are lists.
+    - Some are bulleted.
+       - With different indentation levels.
+          - That look like this.
+  
+  
+  1. And there are numbererd lists too.
+  1. Use just 1s if you want! 
+  1. But the list goes on...
+  - Even if you use dashes or asterisks.
+  * And last but not least, let's not forget embedded images:
+  
+  ![React Logo w/ Text](https://goo.gl/Umyytc)
+  `);
 
   const handleChange = e => {
     setMarkdown(e.target.value);
   };
+
+  let result = marked(markdown);
+
+  function createMarkup() {
+    return { __html: result };
+  }
 
   return (
     <>
@@ -18,7 +84,7 @@ function App() {
         onChange={handleChange}
         value={markdown}
       />
-      <div id='preview'>{markdown}</div>
+      <div id='preview' dangerouslySetInnerHTML={createMarkup()} />
     </>
   );
 }
